34c63d1bff96b8fb3ce3b1f36ce3ca1f6134c942
Kim Freechack <kimtho@ats-inc.com>
2025-08-14 10:37:06 -0500

    demo

--- App.tsx
+++ App.tsx
@@ -5,23 +5,72 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-map-clustering';
+import { Marker } from 'react-native-maps';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function getRandomLatitude(min = 40, max = 48) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomLongitude(min = -95, max = -75) {
+  return Math.random() * (max - min) + min;
+}
+
+const INITIAL_REGION = {
+  latitude: 40,
+  longitude: -95,
+  latitudeDelta: 8,
+  longitudeDelta: 20,
+};
+
+function App(): React.JSX.Element {
+  const _generateMarkers = (count: number) => {
+    const markers = [];
+
+    for (let i = 0; i < count; i++) {
+      markers.push(
+        <Marker
+          key={i}
+          coordinate={{
+            latitude: getRandomLatitude(),
+            longitude: getRandomLongitude(),
+          }}
+        >
+          <View style={styles.customMarker}>
+            <Text style={styles.bold}>{i}</Text>
+          </View>
+        </Marker>
+      );
+    }
+
+    return markers;
+  };
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <MapView initialRegion={INITIAL_REGION} style={styles.map}>
+      {_generateMarkers(50)}
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  map: {
     flex: 1,
+  },
+  customMarker: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ca5cdd",
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+  },
+  bold: {
+    fontWeight: 800,
   },
 });
 
